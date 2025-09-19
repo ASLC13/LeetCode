99f27102db9e45ee2e091eea866e94bdbc4aadf6
ASLC13 <ahmadsepahi13@gmail.com>
2025-09-18 21:52:38 -0700

    Time: 16 ms (13.57%) | Memory: 17.7 MB (88.96%) - LeetSync

--- 9-palindrome-number/palindrome-number.py
+++ 9-palindrome-number/palindrome-number.py
@@ -6,8 +6,8 @@ class Solution:
         while x > 0:
             res = res * 10 + x % 10
             x = x//10
-        if res == y: return True
-        return False
+        
+        return res == y
 
 
         
